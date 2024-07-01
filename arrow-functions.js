958496a8de8ff67d5be69d5504bda5cae59d28a1
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-07-01 10:35:48 +0000

    Add description of arrow functions.

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,43 @@
  */
 
 // Vanilla JavaScript Function
+/**
+function addTwoNumbers(a, b) {
+    return a + b
+}
 
+let sum = addTwoNumbers(9, 5);
+console.log(sum); */
 // Arrow Function With Parameters
+/**
+const addTwoNumbers = (a, b) => {
+    return a + b
+}
 
+let sum = addTwoNumbers(9, 5);
+console.log(sum);
+*/
 // Single Line Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => a + b;
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+
+saySomething('Hello world!');
+
+const sayHello = () => console.log('If you arent passing any function, use paraenthasis so js knows its a function. ');
+sayHello()
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+        This is how you return multiple lines.
+    </p>`
+)
+
+console.log(returnMultipleLines());
